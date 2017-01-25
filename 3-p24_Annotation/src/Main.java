1398f5dd33282e69528bc59f122844c72c034179
ViktorChmel <ext-viktor.chmel@here.com>
2017-01-25 18:17:16 +0200

    [add] notes

--- 3-p24_Annotation/src/Main.java
+++ 3-p24_Annotation/src/Main.java
@@ -2,6 +2,9 @@ public class Main {
 	public static void main(String[] args) {
 
 		System.out.println(Tester.test(SomeClass.class, OtherClass.class));
-
+        // Output
+        //		SomeClass: true
+        //		OtherClass: true
+        //		true // - printed by main function
 	}	
 }
